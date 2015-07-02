b0a5829e6d7f54afdc296ed6fa6ed1309e09e228
kei-sidorov <zGiY1k8ff>
2015-07-03 00:29:48 +0700

    Added distinction by type Implement GCM send function iOS server mode set from config file

--- Notify.php
+++ Notify.php
@@ -10,7 +10,10 @@ class Notify {
 
     private $db;
     private $androidApiKey = "";
-    private $iosKeyFile, $iosCertificateFile, $iosKeyPass;
+    private $iosKeyFile, $iosCertificateFile, $iosKeyPass, $iosMode;
+
+    const TOKEN_TYPE_GCM = 1;
+    const TOKEN_TYPE_APS = 2;
 
     /**
      *  Constructor. Load push settings and init database.
@@ -24,6 +27,7 @@ class Notify {
         $this->iosCertificateFile = $config["push"]["ios-cert-file"];
         $this->iosKeyFile = $config["push"]["ios-key-file"];
         $this->iosKeyPass = $config["push"]["ios-key-pass"];
+        $this->iosMode = ($config["push"]["ios-mode"] == "production") ? APN_PRODUCTION : APN_SANDBOX;
 
         $this->db = db::getInstance();
     }
@@ -31,12 +35,15 @@ class Notify {
     /**
      * Check if token exists in database
      * @param $token
+     * @param $type
      * @return bool
      */
-    private function isTokenExists($token)
+    private function isTokenExists($token, $type)
     {
-        $query = $this->db->db->prepare("SELECT COUNT(1) FROM `tokens` WHERE `key` = :key LIMIT 1");
-        $query->execute(array("key" => $token));
+        $query = $this->db->db->prepare("SELECT COUNT(1) FROM `tokens` WHERE `key` = :key AND `type` = :type LIMIT 1");
+        $query->execute( array( "key" => $token,
+                                "type" => (int) $type )
+                        );
         $result = $query->fetchColumn();
 
         return (bool) $result;
@@ -46,17 +53,26 @@ class Notify {
     /**
      * Add new token to database
      * @param $token
+     * @param $type
      * @return bool
+     * @throws AppException
      */
-    public function registerNewToken($token)
+    public function registerNewToken($token, $type)
     {
-        if ($this->isTokenExists($token))
+        if ($type != self::TOKEN_TYPE_APS || $type != self::TOKEN_TYPE_GCM)
+        {
+            throw new AppException('Invalid token type given');
+        }
+
+        if ($this->isTokenExists($token, $type))
         {
             return false;
         }
 
-        $query = $this->db->db->prepare("INSERT INTO `tokens` SET `key` = :key");
-        $query->execute(array("key" => $token));
+        $query = $this->db->db->prepare("INSERT INTO `tokens` SET `key` = :key, `type` = :type");
+        $query->execute( array( "key" => $token,
+                                "type" => (int) $type )
+                        );
 
         return true;
     }
@@ -66,38 +82,32 @@ class Notify {
      * @param $typeId
      * @param $elementId
      */
-    public function sendData($message, $typeId, $elementId) {
-        $gcmTokens = [];
-        $apsTokens = [];
-
-        foreach($tokens as $token) {
-            if (preg_match('/^[a-f0-9]{64}$/i', $token)) {
-                $apsTokens[] = $token;
-            }else{
-                $gcmTokens[] = $token;
-            }
-        }
+    public function sendData($message, $typeId, $elementId)
+    {
+        $gcmTokens = $this->getTokens(self::TOKEN_TYPE_GCM);
+        $apsTokens = $this->getTokens(self::TOKEN_TYPE_APS);
 
         $this->sendDataToGCM($gcmTokens, $message, $typeId, $elementId);
         $this->sendDataToAPS($apsTokens, $message, $typeId, $elementId);
     }
 
     /**
-     * Sends push to Apple Push Service
+     * Sends push to Apple Push Notification Service (for iOS apps)
      * @param $tokens
      * @param $message
      * @param $typeId
      * @param $elementId
      * @throws AppException
      */
-    private function sendDataToAPS($tokens, $message, $typeId, $elementId) {
+    private function sendDataToAPS($tokens, $message, $typeId, $elementId)
+    {
         $apn = apn_init();
         apn_set_array($apn, array(
             'certificate' => $this->iosCertificateFile,
             'private_key' => $this->iosKeyFile,
             'private_key_pass' => $this->iosKeyPass,
             'tokens' => $tokens,
-            'mode' => APN_PRODUCTION
+            'mode' => $this->iosMode
         ));
 
         $payload = apn_payload_init();
@@ -116,12 +126,76 @@ class Notify {
                 throw new AppException('Could not sent push notification: ' . $error);
             }
         } else {
-            throw new AppException('Could not connected to Apple Push Notification Servece: ' . $error);
+            throw new AppException('Could not connected to Apple Push Notification Service: ' . $error);
         }
 
         apn_close($apn);
         apn_payload_free($payload);
         apn_free($apn);
+    }
+
+    /**
+     * Sends push via Google Cloud Messaging (for Android apps)
+     * @param $tokens
+     * @param $message
+     * @param $typeId
+     * @param $elementId
+     */
+    private function sendDataToGCM($tokens, $message, $typeId, $elementId)
+    {
+
+        $headers = array
+        (
+            'Authorization: key=' . $this->androidApiKey,
+            'Content-Type: application/json'
+        );
+
+        $payloadMessage = array
+        (
+            'message' 		=> $message,
+            'title'			=> $message,
+            'subtitle'		=> '',
+            'tickerText'	=> '',
+            'vibrate'	=> 1,
+            'sound'		=> 1,
+            'listType'  => $typeId,
+            'obj' => $elementId
+        );
+
+        $payload = array
+        (
+            'registration_ids' => $tokens,
+            'data' => $payloadMessage
+        );
+
+        $ch = curl_init();
+        curl_setopt($ch, CURLOPT_URL, 'https://android.googleapis.com/gcm/send' );
+        curl_setopt($ch, CURLOPT_POST, true );
+        curl_setopt($ch, CURLOPT_HTTPHEADER, $headers );
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, true );
+        curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false );
+        curl_setopt($ch, CURLOPT_POSTFIELDS, json_encode( $payload ) );
+        curl_exec($ch );
+
+    }
+
+    /**
+     * Request token with some type from DB
+     * @param $type
+     * @return array
+     */
+    private function getTokens($type)
+    {
+        $query = $this->db->db->prepare("SELECT `key` FROM `tokens` WHERE `type` = :type");
+        $query->execute( array("type" => $type ) );
+        $data = Array();
+
+        while ($row = $query->fetch(PDO::FETCH_ASSOC))
+        {
+            $data[] = $row['key'];
+        }
+
+        return $data;
     }
 
 
