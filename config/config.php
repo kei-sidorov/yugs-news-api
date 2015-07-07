027cb5a766b51772ff5215177e21aa87ac1b54ca
Kirill Sidorov <web.sidorov@gmail.com>
2015-07-07 15:26:37 +0700

    Fix after structure update

--- config/config.php
+++ config/config.php
@@ -1,13 +1,13 @@
 <?php
 
-require_once('../core/AppException.php');
-require_once('../core/Database.php');
-require_once('../core/TextCleaner.php');
-require_once('../core/Notify.php');
-require_once('../core/Router.php');
+require_once('core/AppException.php');
+require_once('core/Database.php');
+require_once('core/TextCleaner.php');
+require_once('core/Notify.php');
+require_once('core/Router.php');
 
-require_once('../core/news/NewsCollectionInterface.php');
-require_once('../core/news/NewsCollection.php');
-require_once('../core/news/NewsCollectionUMI.php');
+require_once('core/news/NewsCollectionInterface.php');
+require_once('core/news/NewsCollection.php');
+require_once('core/news/NewsCollectionUMI.php');
 
-require_once('../../standalone.php');
+require_once('../standalone.php');
